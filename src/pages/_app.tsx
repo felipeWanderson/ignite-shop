debf84d7c3f91094a4b7dec5a651547f4957e102
Felipe Leal <felipe.leal@compasso.com.br>
2023-03-23 21:17:59 -0300

    structure buttom cart

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { globalStyles } from '@/styles/global'
-import { Container, Header } from '@/styles/pages/app'
+import { CartContainer, CartNotfication, Container, Header } from '@/styles/pages/app'
+import { Handbag } from '@phosphor-icons/react'
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
 import logoIgniteShop from '../assets/logo.svg'
@@ -11,6 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <Image src={logoIgniteShop} alt=""  />
+        <CartContainer>
+          <Handbag size={24} weight="bold" />
+          <CartNotfication>1</CartNotfication>
+        </CartContainer>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -12,5 +12,48 @@ export const Header = styled('header', {
   padding: '2rem 0',
   width: '100%',
   maxWidth: 1180,
-  margin: '0 auto'
+  margin: '0 auto',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between'
+})
+
+export const CartContainer = styled('button', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: 48,
+  height: 48,
+  background: '$gray800',
+
+  borderRadius: 6,
+  border: 'none',
+  cursor: 'pointer',
+
+  position: 'relative',
+
+  svg: {
+    fill: '$gray300',
+  }
+});
+
+export const CartNotfication = styled('span', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  width: 24,
+  height: 24,
+  border:'3px solid #121214',
+  background: '$green300',
+  borderRadius: '100%',
+
+  position: 'absolute',
+  left: 31,
+  top: -7,
+
+  fontWeight: 700,
+  fontSize: '$sm',
+  color: '$white'
 })
